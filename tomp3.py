03fa6a8cf60559a758b53151f48fd31dcd8cf246
marichka-dobko <dobko_m@ucu.edu.ua>
2023-05-26 22:22:30 -0400

    mp3

---
+++ tomp3.py
@@ -0,0 +1,23 @@
+import pydub
+import numpy as np
+import pandas as pd
+
+
+def write(f, sr, x, normalized=False):
+    """numpy array to MP3"""
+    channels = 2 if (x.ndim == 2 and x.shape[1] == 2) else 1
+    if normalized:  # normalized array - each item should be a float in [-1, 1)
+        y = np.int16(x * 2 ** 15)
+    else:
+        y = np.int16(x)
+    song = pydub.AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
+    song.export(f, format="mp3", bitrate="320k")
+
+
+sr = 44100
+
+df = pd.read_csv('wave.csv')
+coords = df.index
+values = df['signal']
+
+write('out2.mp3', sr, values)
